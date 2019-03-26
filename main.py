20013fa1dd8b5c2652e9158b28e8338d5a5d7c29
riceboat <gregorwatt1@gmail.com>
2019-03-26 19:36:24 +0000

    More efficent, non spaghetti code version
    Added a better UI
    Automatic domain finding
    will soon add a way to store username and password encrypted in a text file using the first two functions

--- main.py
+++ main.py
@@ -1,43 +1,78 @@
 from pexpect import pxssh
-import os,nmap
-s=pxssh.pxssh()
+import os,nmap,socket,struct,fcntl
 cport=8022
-suffixes=[]
-done=False
-def command(s,cmd):
- print("Excecuting>>> "+cmd)
- s.sendline(cmd)
-prefix=str(raw_input("Please enter the first three letters of your ip address or press enter for last used: "))
-comd=str(raw_input("Please enter the command(s) you want to run: "))
-out=str(raw_input("Display command output? y/n: "))
-if out=="y":
- output=True
-else:
- output=False
-if prefix=="":
- f=open("lastused.txt","r")
- prefix=f.readline().strip()
-else:
- if os.path.isfile('lastused.txt'):
-  os.system("rm lastused.txt")
- f=open("lastused.txt","w")
- f.write(prefix)
-f.close()
-print("scanning for bots on "+prefix)
-scanner=nmap.PortScanner()
-scanner.scan(prefix+".0/24",str(cport))
-suffixes=scanner.all_hosts()
-for ip in suffixes:
- s=pxssh.pxssh()
- try:
-    print("Connecting to > "+ip+"              *")
-    s.force_password=True
-    s.login(ip,"root","raspberry",port=cport)
-    print("Logged in to "+ip+"                 *")
-    command(s,comd)
-    s.prompt()
-    if out:
-     print(s.before)
-    s.logout()
- except:
-  print("Host is down^")
+
+def decryptor(stri,key):
+ res=""
+ key=key*100
+ for x in range(len(stri)):
+  res=res+chr((ord(stri[x])-96)-(ord(key[x])-96)+96)
+ return res
+
+def encryptor(stri,key):
+ res=""
+ key=key*100
+ for x in range(len(stri)):
+  res=res+chr((ord(stri[x])-96)+(ord(key[x])-96)+96)
+ return res
+
+class bot:
+ def __init__(self,ip):
+  self.ip=ip
+
+ def spawn(self):
+  try:
+   login=True
+   self.session=pxssh.pxssh()
+   self.session.login(self.ip,"u0_a213","raspberry",port=cport)
+   print("Logged into "+self.ip)
+  except:
+   login=False
+   print("Login failed for "+self.ip)
+  return login
+
+ def cmd(self,com,out=False):
+  try:
+   self.session.sendline(com)
+   if out:
+    self.session.prompt()
+    print(self.session.before)
+  except:
+   print(com+" Failed on "+self.ip)
+
+def getmyip(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', ifname[:15]))[20:24])
+
+def search(myip):
+ print("My ip address is "+myip)
+ suf=""
+ skip=[]
+ domain=myip[:myip.rfind(".")]
+ while suf != "":
+  suf=str(raw_input("Please enter any ip suffixes you want to skip:"))
+  skip.append(suf)
+ print("Scanning for bots on "+domain)
+ scanner=nmap.PortScanner()
+ scanner.scan(domain+".0/24",str(cport))
+ suffixes=scanner.all_hosts()
+ print("Found "+str(len(suffixes))+" hosts with "+str(cport)+" open!")
+ return suffixes
+
+def sendall(bots,cmd,out=True):
+ for b in bots:
+  b.cmd(cmd,out)
+
+def connect(ips):
+ bots=[]
+ for ip in ips:
+  tmp=bot(ip)
+  if tmp.spawn()==True:
+   bots.append(tmp)
+ return bots
+
+bots=connect(search(getmyip("wlan0")))
+print(str(len(bots))+" active")
+while len(bots) >0:
+  sendall(bots,str(raw_input(">>>")))
+print("All bots offline")
